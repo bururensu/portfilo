8c91c32db8dd26f910cf1ceef66fcc0c14c8a5e4
bururensu <60644377+bururensu@users.noreply.github.com>
2022-03-13 12:50:31 +0900

    Add files via upload

---
+++ js/portfilo.js
@@ -0,0 +1,37 @@
+
+
+//ハンバーガmenu
+$('.menu').on('click',function(){
+$('.menu__line').toggleClass('active');
+$('.gnav').fadeToggle();
+});
+
+
+//棒グラフ
+$(document).ready(function() {
+function skillSet() {
+$('.bar-info').each(function() {
+total = $(this).data("total");
+$(this).css("width", total + "%");
+});
+
+$('.percent').each(function() {
+var $this = $(this);
+$({
+Counter: 10
+}).animate({
+Counter: $this.text()
+}, {
+duration: 3000,
+easing: 'swing',
+step: function() {
+  $this.text(Math.ceil(this.Counter) + "%");
+}
+});
+});
+};
+setTimeout(skillSet, 1000);
+});
+
+
+const swiper = new Swiper(".swiper", {});
